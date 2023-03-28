e07959f90b2d5a84bd719195c2e405ccaa29e98f
aguiroldanns <aguiroldanns@gmail.com>
2023-03-28 18:33:48 +0200

    first test

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 Node JS unit testing with Mocha
+
+- First you need to install de dependencies with - npm install
+- To run the test you need to use - npm run test
--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -1,2 +1,11 @@
+var chai = require("chai");
+var expect = chai.expect;
+
 var MyClass = require("../src/myClass.js");
 var myObj = new MyClass();
+
+describe("Test suit", () => {
+  it("Test the add method", () => {
+    expect(myObj.add(1, 2)).to.be.equal(3);
+  });
+});
